c6fecaa752338cd839ebfdf7dd11b6434b33c4ae
markus_mayerle <markus.mayerle@wk-it.com>
2020-10-02 13:36:37 +0200

    Add script service

--- transpile.sh
+++ transpile.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+docker pull quay.io/coreos/fcct:release
 docker run -i --rm quay.io/coreos/fcct:release --pretty --strict < example.fcc > example.ign
--- validate.sh
+++ validate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+docker pull quay.io/coreos/ignition-validate:release
 docker run -it --rm -v ${PWD}:/pwd -w /pwd quay.io/coreos/ignition-validate:release example.ign && echo "Success!"
