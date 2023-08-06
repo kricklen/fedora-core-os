28ef05a641aeab7ad935ccb10d00160036816b45
kricklen <dirion@gmx.net>
2023-08-06 17:18:47 +0200

    Test new bu version

--- transpile.sh
+++ transpile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker pull quay.io/coreos/fcct:release
-docker run -i --rm quay.io/coreos/fcct:release --pretty --strict < example.fcc > example.ign
+docker pull quay.io/coreos/butane:release
+docker run -i --rm quay.io/coreos/butane:release --pretty --strict < example.bu > example.ign
